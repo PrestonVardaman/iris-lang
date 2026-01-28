87f988ce1185f62e8c7655d914f7d296516f7835
PrestonVardaman <prestonvardaman1@gmail.com>
2026-01-27 18:09:19 -0600

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/*
+Authored by: Preston Vardaman
+Iris Compiler Project
+*/
+
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+enum class(){
+	
+
+
+
+
+}
+
+
+
+
+int main(){
+
+
+
+}
